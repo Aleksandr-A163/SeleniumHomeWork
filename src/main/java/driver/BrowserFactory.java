ef46edc59eb91dee09fc97c799ef7534924d4751
Aleksandr Anosov <you@example.com>
2025-05-16 18:15:52 +0300

    update BrowserFactory

--- src/main/java/driver/BrowserFactory.java
+++ src/main/java/driver/BrowserFactory.java
@@ -1,36 +1,12 @@
 package driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 
 public class BrowserFactory {
 
     public static WebDriver create() {
         String browserProperty = System.getProperty("browser");
         BrowserType browser = BrowserType.from(browserProperty);
-
-        switch (browser) {
-            case FIREFOX:
-                WebDriverManager.firefoxdriver().setup();
-                System.out.println("[INFO] Используется браузер: Firefox");
-                return new FirefoxDriver();
-
-            case EDGE:
-                WebDriverManager.edgedriver().setup();
-                System.out.println("[INFO] Используется браузер: Edge");
-                return new EdgeDriver();
-
-            case CHROME:
-            default:
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--start-maximized");
-                System.out.println("[INFO] Используется браузер: Chrome");
-                return new ChromeDriver(options);
-        }
+        return browser.createDriver();
     }
 }
--- src/main/java/driver/BrowserType.java
+++ src/main/java/driver/BrowserType.java
@@ -1,9 +1,44 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 public enum BrowserType {
-    CHROME,
-    FIREFOX,
-    EDGE;
+    CHROME {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            return new ChromeDriver(options);
+        }
+    },
+    FIREFOX {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--start-maximized");
+            return new FirefoxDriver(options);
+        }
+    },
+    EDGE {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--start-maximized");
+            return new EdgeDriver(options);
+        }
+    };
+
+    public abstract WebDriver createDriver();
 
     public static BrowserType from(String name) {
         if (name == null || name.isBlank()) {
